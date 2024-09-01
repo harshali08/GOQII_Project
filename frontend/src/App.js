c3c66845551660028e17657450a46de4c997594f
harshali <harshalifarde@.com>
2024-09-01 14:32:17 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddUser from './Components/AddUser'
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='font-bold text-red-800'>Hello World</h2>
+      <AddUser/>
+    </div>
+  )
+}
+
+export default App
